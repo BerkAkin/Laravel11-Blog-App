e340a7d92f11a8ec0294fbb994342da4991fbac6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-31 20:28:25 +0300

    auth routes are now closured with group and prefix to adjust readability of code

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PostController::class,'index']);
+
+Route::group(['prefix'=> 'auth'], function(){
+    Auth::routes();
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
