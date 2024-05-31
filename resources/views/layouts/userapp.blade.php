fcd9c8c8ff404aac40d47dbece9896d629da76ca
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-01 02:49:59 +0300

    refactor: styling codes of userapp page divided into layouts

---
+++ resources/views/layouts/userapp.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>EnyThing Yönetim Paneli</title>
+        <link rel="stylesheet" href="{{ asset('vendors/feather/feather.css') }}">
+        <link rel="stylesheet" href="{{ asset('/vendors/mdi/css/materialdesignicons.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/vendors/ti-icons/css/themify-icons.css') }}">
+        <link rel="stylesheet" href="{{ asset('/vendors/typicons/typicons.css') }}">
+        <link rel="stylesheet" href="{{ asset('/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+        <link rel="stylesheet" href="{{ asset('/vendors/css/vendor.bundle.base.css') }}">
+        <link rel="stylesheet" href="{{ asset('/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/js/select.dataTables.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css') }}">
+        <link rel="shortcut icon" href="{{ asset('/images/favicon.png') }}" />
+    </head>
+
+    <body class="with-welcome-text">
+    <div class="container-scroller">
+        <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+            <div class="me-3">
+                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-bs-toggle="minimize">
+                    <span class="icon-menu"></span>
+                </button>
+            </div>
+            <div>
+                <a class="navbar-brand" href="../index.html">
+                    <img src="images/logo5.png" alt="logo" />
+                </a>
+            </div>
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-top">
+            <ul class="navbar-nav">
+            <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
+                <h1 class="welcome-text">Hoş Geldiniz, <span class="text-black fw-bold">John Doe</span></h1>
+            </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+            <li class="nav-item d-none d-lg-block">
+                <div id="datepicker-popup" class="input-group date datepicker navbar-date-picker">
+                <span class="input-group-addon input-group-prepend border-right">
+                    <span class="icon-calendar input-group-text calendar-icon"></span>
+                </span>
+                <input type="text" class="form-control">
+                </div>
+            </li>
+            <li class="nav-item">
+                <form class="search-form" action="#">
+                <i class="icon-search"></i>
+                <input type="search" class="form-control" placeholder="Search Here" title="Search here">
+                </form>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link count-indicator" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
+                <i class="icon-bell"></i>
+                <span class="count"></span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0"
+                aria-labelledby="notificationDropdown">
+                <a class="dropdown-item py-3 border-bottom">
+                    <p class="mb-0 font-weight-medium float-left">You have 4 new notifications </p>
+                    <span class="badge badge-pill badge-primary float-right">View all</span>
+                </a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link count-indicator" id="countDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="icon-mail icon-lg"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0"
+                aria-labelledby="countDropdown">
+                <a class="dropdown-item py-3">
+                    <p class="mb-0 font-weight-medium float-left">You have 7 unread mails </p>
+                    <span class="badge badge-pill badge-primary float-right">View all</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                    <img src="../assets/images/faces/face1.jpg" alt="image" class="img-sm profile-pic">
+                    </div>
+                    <div class="preview-item-content flex-grow py-2">
+                    <p class="preview-subject ellipsis font-weight-medium text-dark">Travis Jenkins </p>
+                    <p class="fw-light small-text mb-0"> The meeting is cancelled </p>
+                    </div>
+                </a>
+                </div>
+            </li>
+            <li class="nav-item dropdown d-none d-lg-block user-dropdown">
+                <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                <img class="img-xs rounded-circle" src="../assets/images/faces/face8.jpg" alt="Profile image"> </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
+                <div class="dropdown-header text-center">
+                    <img class="img-md rounded-circle" src="../assets/images/faces/face8.jpg" alt="Profile image">
+                    <p class="mb-1 mt-3 font-weight-semibold">Allen Moreno</p>
+                    <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
+                </div>
+                <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My
+                    Profile <span class="badge badge-pill badge-danger">1</span></a>
+                <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i>
+                    Messages</a>
+                <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a>
+                </div>
+            </li>
+            </ul>
+            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+            data-bs-toggle="offcanvas">
+            <span class="mdi mdi-menu"></span>
+            </button>
+        </div>
+        </nav>
+        <div class="container-fluid page-body-wrapper">
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                    <ul class="nav">
+                        <li class="nav-item">
+                        <a class="nav-link" href="../index.html">
+                            <i class="mdi mdi-grid-large menu-icon"></i>
+                            <span class="menu-title">Dashboard</span>
+                        </a>
+                        </li>
+                    </ul>
+            </nav>
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('vendors/progressbar.js/progressbar.min.js') }}"></script>
+    <script src="{{ asset('js/off-canvas.js') }}"></script>
+    <script src="{{ asset('js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('js/template.js') }}"></script>
+    <script src="{{ asset('js/settings.js') }}"></script>
+    <script src="{{ asset('js/todolist.js') }}"></script>
+    <script src="{{ asset('js/jquery.cookie.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+    <script src="{{ asset('js/proBanner.js') }}"></script>
+    </body>
+
+</html>
