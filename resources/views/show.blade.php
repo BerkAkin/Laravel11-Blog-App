50fc87d2d6abf7e7ea4c694b70da26f3ac13ce6e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-31 21:20:57 +0300

    created: detail blade file to show post details

---
+++ resources/views/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="w3-row">
+    <div class="w3-col l8 s12">
+        <div class="w3-card-4 w3-margin w3-white">
+            <img src="https://placehold.jp/3d4070/ffffff/600x120.png" alt="Nature" style="width:100%">
+            <div class="w3-container">
+              <h3><b>{{ $posts->title }}</b></h3>
+              <h5>Yazar <span class="w3-opacity">{{ $posts->created_at }}</span></h5>
+            </div>
+    
+            <div class="w3-container">
+              <p>{{ $posts->body }}</p>
+          </div>
+    </div>
+</div>
+@endsection
