2a623459235e1ef87459ad63939ec2bc3d48a560
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-31 20:50:03 +0300

    feat: added posts route to postcontroller route group for fetching data

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::get('home',[PostController::class,'index']);
 Route::get('/', [PostController::class,'index']);
 
 Route::group(['prefix'=> 'auth'], function(){
     Auth::routes();
+});
+
+Route::controller(PostController::class)->group(function(){
+    Route::get('posts','index');
 });
 
 
