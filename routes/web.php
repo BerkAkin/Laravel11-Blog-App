119a397fa713c6b34c96aed36606aa57c2ecd53d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-01 03:01:57 +0300

    created: logout route for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('home',[HomeController::class,'index']);
 Route::get('/', [PostController::class,'index']);
 
 Route::group(['prefix'=> 'auth'], function(){
+    Route::get('logout',[UserController::class,'logout']);
     Auth::routes();
 });
 
