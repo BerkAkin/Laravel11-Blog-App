84479753004cb6a5e65eae2b472c9ac4f6c8ced1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-03 21:11:04 +0300

    feat: post image can now be shown

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,7 +6,7 @@
 <div class="w3-row">
     <div class="w3-col l8 s12">
         <div class="w3-card-4 w3-margin w3-white">
-            <img src="https://placehold.jp/3d4070/ffffff/600x120.png" alt="Nature" style="width:100%">
+            <img src="{{ asset('storage/images/'.$posts->photo) }}" alt="Nature" style="width:100%">
             <div class="w3-container">
               <h3><b>{{ $posts->title }}</b></h3>
               <h5>Yazar <span class="w3-opacity">{{ $posts->created_at }}</span></h5>
