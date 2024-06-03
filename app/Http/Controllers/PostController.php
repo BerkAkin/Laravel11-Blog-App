5e054a7dba46ad24bf09fee66cd7601bb73e9e94
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-03 21:10:51 +0300

    refactor: post addition and deleteion functions moved out to Homecontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
+
 use App\Models\Posts;
 
 class PostController extends Controller
@@ -19,15 +19,5 @@ class PostController extends Controller
        return view('show',compact('posts'));
     }
 
-    public function create(Request $request){
-        $post = new Posts();
-        $post->title= $request->title;
-        $post->body= $request->body;
-        $post->slug= Str::slug($request->title,'-','tr');
-        $post->photo= $request->file('image')->getClientOriginalName();
-        $path = $request->file('image')->store('public/images');
-        $post->author_id= Auth::user()->id;
-        $post->save();
-        return redirect('home');
-    }
+
 }
