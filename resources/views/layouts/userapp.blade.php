65f6b156d7ba5cd475dc9f915ed8d6eb7c47ef1c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-03 21:11:24 +0300

    style: added fa

--- resources/views/layouts/userapp.blade.php
+++ resources/views/layouts/userapp.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('/js/select.dataTables.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css') }}">
         <link rel="shortcut icon" href="{{ asset('/images/favicon.png') }}" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
 
     <body class="with-welcome-text">
