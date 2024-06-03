0f787da3030b480aa4730c9b4112801f76f1b7d2
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-03 05:52:20 +0300

    feat: post saving route implemented

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
 Route::get('home',[HomeController::class,'index'])->name('userhome');
+Route::post('home',[PostController::class,'create']);
+
 Route::get('/', [PostController::class,'index'])->name('home');
 
 Route::group(['prefix'=> 'auth'], function(){
