a53f3c365ac394a216ec3d6015ff92ddc9ff6458
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-04 02:09:26 +0300

    feat: added update post route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('home',[HomeController::class,'index'])->name('userhome');
 Route::post('home',[HomeController::class,'create']);
 Route::get('post/delete/{id}',[HomeController::class,'delete'])->name('postdelete');
 Route::get('post/edit/{id}',[HomeController::class,'edit'])->name('postedit');
+Route::post('post/update',[HomeController::class,'update'])->name('postupdate');
 
 Route::get('/', [PostController::class,'index'])->name('home');
 
