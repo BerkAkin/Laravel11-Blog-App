65fc8ff95b17a7b985d119a587b0b5bcbaad647b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-03 05:56:44 +0300

    style: changed default background color of main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,7 +77,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4" style="background: #f4f5f7">
             @yield('content')
         </main>
     </div>
