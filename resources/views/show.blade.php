2331df619fd523a64dedff3e284f5eca259edbdf
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-04 02:59:32 +0300

    refactor: ui sectionized

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.app')
 
+@section('right')
+@parent
+@endsection
+
 @section('content')
 
 
-<div class="w3-row">
+    <h3><b>{{ $posts->title }}</b></h3>
     <div class="w3-col l8 s12">
         <div class="w3-card-4 w3-margin w3-white">
             <img src="{{ asset('storage/images/'.$posts->photo) }}" alt="Nature" style="width:100%">
             <div class="w3-container">
-              <h3><b>{{ $posts->title }}</b></h3>
-              <h5>Yazar <span class="w3-opacity">{{ $posts->created_at }}</span></h5>
+              <h5>{{ $posts->author->name }} <span class="w3-opacity">{{ $posts->created_at }}</span></h5>
             </div>
     
             <div class="w3-container">
               <p>{{ $posts->body }}</p>
           </div>
+      </div>
     </div>
-</div>
 @endsection
