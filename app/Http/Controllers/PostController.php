d3fef0ac8b0ab0edd482b0a935a64fe706ae91fc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-03 05:51:14 +0300

    feat: posts can now be stored in database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Posts;
 
 class PostController extends Controller
@@ -15,5 +16,16 @@ class PostController extends Controller
     public function show($slug){
        $posts = Posts::where('slug',$slug)->get()->first();
        return view('show',compact('posts'));
+    }
+
+    public function create(Request $request){
+        $post = new Posts();
+        $post->title= $request->title;
+        $post->body= $request->body;
+        $post->slug= $request->slug;
+        $post->photo= "";
+        $post->author_id= Auth::user()->id;
+        $post->save();
+        return redirect('home');
     }
 }
