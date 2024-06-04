a56eb630c254fe9c84f6e48d3c251d7504a622b2
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-04 03:50:37 +0300

    style: minor improvements on alignment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,15 +18,15 @@
           @endif
           
         <div class="w3-container">
-          <h3><b><a href="{{ $ps->slug }}">{{ $ps->title }}</a></b></h3>
-          <h5>Oluşturan: {{ $ps->author->name }} <span class="w3-opacity">{{ $ps->created_at->diffForHumans() }}</span></h5>
+          <h3 class="mt-3"><b><a style="text-decoration: none; color:black" href="{{ $ps->slug }}">{{ $ps->title }}</a></b></h3>
+          <h5 class="mt-3">Oluşturan: {{ $ps->author->name }} <span class="w3-opacity">{{ $ps->created_at->diffForHumans() }}</span></h5>
         </div>
 
-        <div class="w3-container">
-          <p>{{Str::substr( $ps->body,0,250) }}...</p>
-          <div class="w3-row">
+        <div class="w3-container mt-3">
+          <p style="font-size: 1.15rem">{{Str::substr( $ps->body,0,250) }}...</p>
+          <div class="w3-row mt-4 mb-2">
             <div class="w3-col m8 s12">
-              <p><button class="w3-button w3-padding-large w3-white w3-border"><b><a href="{{ $ps->slug }}">DEVAMINI OKU »</a> </b></button></p>
+              <p ><button class="w3-button w3-padding-large w3-white w3-border"><b><a style="text-decoration: none; color: black" href="{{ $ps->slug }}">DEVAMINI OKU »</a> </b></button></p>
             </div>
             <div class="w3-col m4 w3-hide-small">
               <p><span class="w3-padding-large w3-right"><b>Yorumlar  </b> <span class="w3-tag">0</span></span></p>
@@ -34,7 +34,6 @@
           </div>
         </div>
       </div>
-      <hr>
     @endforeach
     </div>
 
