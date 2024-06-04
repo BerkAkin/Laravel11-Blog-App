31b4bc3e09f9bf31ff7a7ba7cd02f1eca25439df
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-04 03:50:34 +0300

    style: minor improvements on alignment

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -41,16 +41,19 @@
 @section('content')
 
 
-    <h3><b>{{ $posts->title }}</b></h3>
+
     <div class="w3-col l8 s12">
         <div class="w3-card-4 w3-margin w3-white">
             <img src="{{ asset('storage/images/'.$posts->photo) }}" alt="Nature" style="width:100%">
             <div class="w3-container">
-              <h5>{{ $posts->author->name }} <span class="w3-opacity">{{ $posts->created_at->diffForHumans() }}</span></h5>
+              <h3 class="mt-3"><b>{{ $posts->title }}</b></h3>
+              <h5 class="mt-4">Oluşturan: {{ $posts->author->name }} 
+                <span class="w3-opacity">{{ $posts->created_at->diffForHumans() }}</span>
+              </h5>
             </div>
     
-            <div class="w3-container">
-              <p>{{ $posts->body }}</p>
+            <div class="w3-container mt-3">
+              <p style="font-size: 1.15rem">{{ $posts->body }}</p>
           </div>
         </div>
 
