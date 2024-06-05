d5394cb6e67a2967833150e8817ebce2c2e6e13d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-05 06:04:32 +0300

    feat: added user's post and comment counts relation to user model file

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -43,5 +44,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function posts(){
+        return $this->hasMany(Posts::class,'author_id');
+    }
+
+    
+    public function comments(){
+        return $this->hasMany(Comments::class,'from_user');
     }
 }
