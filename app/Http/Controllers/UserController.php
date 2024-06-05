000c05b1d14f112bceaa078f3de297ee4954fe02
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-05 05:51:42 +0300

    feat: users now can be shown

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -13,10 +14,27 @@ class UserController extends Controller
         $this->middleware('auth');
     }
 
+
+    public function show(){
+        $users = User::all();
+        return View('users.show',compact('users'));
+    }
+
     public function logout(Request $request){
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return Redirect('/');
+    }
+
+    public function delete($id){
+        $user = User::find($id);
+        $user->delete();
+        return redirect('users');
+    }
+
+    public function edit($id){
+        $user = User::find($id);
+        return view('users.edit',compact('user'));
     }
 }
