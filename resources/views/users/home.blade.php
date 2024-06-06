25b10c920690fabe1370f3e5666f15331015c2b2
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-06 08:04:22 +0300

    feat: home screen is now includes simple information

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -9,7 +9,23 @@
             <div class="tab-content tab-content-basic">
             <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
                 <div class="row">
-
+                    <div class="col-lg-12 d-flex flex-column">
+                        <div class="row flex-grow">
+                        <div class="col-12 grid-margin stretch-card">
+                            <div class="card">
+                            <div class="card-body">
+                                <div class="d-sm-flex justify-content-between align-items-start">
+                                    <div>
+                                        <h4 class="card-title">Bilgileriniz</h4>
+                                        <p class="card-description">Rolünüz: {{ Auth::user()->role }}</p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
                  
                 </div>
                 </div> 
