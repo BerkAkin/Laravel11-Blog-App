08719088dde5cc14854b020252705acee62a115b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-06 05:03:09 +0300

    created: post lists is now moved to separated posts blade

---
+++ resources/views/users/posts.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.userapp')
+
+@section('content')
+       <div class="col-lg-12 d-flex flex-column">
+        <a href="{{ route('postcreate') }}" class="btn btn-success fs-5">Haber Ekle</a>
+                        <div class="row flex-grow">
+                            <div class="col-12 grid-margin stretch-card">
+                                <div class="card">
+                                    <div class="card-body">
+                                    <h4 class="card-title">Haberler</h4>
+                                    <div class="table-responsive">
+                                        <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                            <th>#</th>
+                                            <th>Başlık</th>
+                                            <th>Yazar</th>
+                                            <th>Tarih</th>
+                                            <th>Habere Git</th>
+                                            <th>İşlemler</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ( $posts as $post)
+                                            <tr>
+                                                <td>{{$post->id }}</td>
+                                                <td>{{Str::substr( $post->title,0,130) }}...</td>
+                                                <td>{{$post->author->name }}</td>
+                                                <td>{{$post->created_at }}</td>
+                                                <td><a href="{{$post->slug}}">{{Str::substr( $post->slug,0,20)}}...</a></td>
+                                                <td>
+                                                    <div>
+                                                        <a class="btn btn-info btn-sm" href="{{ route('postedit',['id'=>$post->id]) }}"><i class="fa fa-pencil"></i></a>
+                                                        <a onclick="return confirm('Silmek istediğinize emin misiniz?')" class="btn btn-sm btn-danger" href="{{ route('postdelete',['id'=>$post->id ])}}"><i class="fa fa-trash"></i></a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                            
+                                        </tbody>
+                                        </table>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+@endsection
