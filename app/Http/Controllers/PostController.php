49b96d9ac1ba4bb4acf62cbce34fa7d5075d2370
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-06 05:01:45 +0300

    refactor: ckeditor function is now moved to homecontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,19 +38,7 @@ class PostController extends Controller
         return redirect()->back();
     }
 
-    public function ckeditorupload(Request $request){
-        if($request->hasFile('upload')){
-            $originName = $request->file('upload')->getClientOriginalName();
-            $fileName = pathinfo($originName, PATHINFO_FILENAME);
-            $extension = $request->file('upload')->getClientOriginalExtension();
-            $fileName = $fileName . '_' . time() . '.' . $extension;
-
-            $request->file('upload')->move(public_path('storage/images/posts'), $fileName);
-
-            $url = asset('storage/images/posts/' . $fileName);
-            return response()->json(['fileName' => $fileName, 'uploaded'=> 1, 'url' => $url]);
-        }
-    }
+   
 
     public function logout(Request $request){
         Auth::logout();
