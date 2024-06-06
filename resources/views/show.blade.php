8ddb866f5baa69ca44e13b4714a51194c9d2e4d1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-06 19:11:37 +0300

    feat: popular posts section now removed from post detail

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -42,7 +42,7 @@
 
 
 
-    <div class="w3-col l8 s12">
+    <div class="w3-col l12 s12">
         <div class="w3-card-4 w3-margin w3-white">
             <img src="{{ asset('storage/images/'.$posts->photo) }}" alt="Nature" style="width:100%">
             <div class="w3-container">
