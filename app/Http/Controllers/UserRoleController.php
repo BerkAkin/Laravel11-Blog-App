a4bdf783d268df29e19881cc0cfe49c4ec79e0ef
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-06 08:01:14 +0300

    refactor: postcomment moved from postcontroller to here for cleaner code

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Comments;
 
 class UserRoleController extends Controller
 {
@@ -14,4 +16,24 @@ class UserRoleController extends Controller
     public function index(){   
         return view('users.home');
     }
+
+    public function postComment(Request $request){
+        $comment = new Comments();
+        $comment->on_post = $request->postId;
+        $comment->from_user = Auth::user()->id;
+        $comment->body= $request->comment;
+        $comment->save();
+        return redirect()->back();
+    }
+
+    
+    public function yorumSil($id){
+        $comments = Comments::find($id);
+        if(Auth::user()->id == $comments->from_user or Auth::user()->role=='admin'){
+            $comments->delete();
+        }
+
+        return redirect()->back();
+    }
+
 }
