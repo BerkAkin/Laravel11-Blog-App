78f540e6e8169bc0fa0d6d786871b05d7ce33162
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-06 08:00:56 +0300

    fix: postcomment moved from

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,25 +20,6 @@ class PostController extends Controller
        return view('show',compact('posts'));
     }
 
-    public function postComment(Request $request){
-        $comment = new Comments();
-        $comment->on_post = $request->postId;
-        $comment->from_user = Auth::user()->id;
-        $comment->body= $request->comment;
-        $comment->save();
-        return redirect()->back();
-    }
-
-    public function yorumSil($id){
-        $comments = Comments::find($id);
-        if(Auth::user()->id == $comments->from_user ){
-            $comments->delete();
-        }
-
-        return redirect()->back();
-    }
-
-   
 
     public function logout(Request $request){
         Auth::logout();
