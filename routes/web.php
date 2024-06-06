c6be40448a0d265978c4fb22fa0f7953ee65c1f3
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-06-06 04:25:09 +0300

    feat: user routes are now protected with admin middleware

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckAdmin;
+
+
+Route::middleware([CheckAdmin::class])->group(function(){
+    Route::get('users',[UserController::class,'show'])->name('users');
+    Route::get('users/edit/{id}',[UserController::class,'edit'])->name('useredit');
+    Route::get('users/delete/{id}',[UserController::class,'delete'])->name('userdelete');
+    Route::get('users/create',[UserController::class,'create'])->name('usercreate');
+    Route::post('users/store',[UserController::class,'store'])->name('userstore');
+
+});
 
 Route::get('home',[HomeController::class,'index'])->name('userhome');
 Route::post('home',[HomeController::class,'create']);
@@ -12,11 +23,7 @@ Route::get('post/edit/{id}',[HomeController::class,'edit'])->name('postedit');
 Route::post('post/update',[HomeController::class,'update'])->name('postupdate');
 
 
-Route::get('users',[UserController::class,'show'])->name('users');
-Route::get('users/edit/{id}',[UserController::class,'edit'])->name('useredit');
-Route::get('users/delete/{id}',[UserController::class,'delete'])->name('userdelete');
-Route::get('users/create',[UserController::class,'create'])->name('usercreate');
-Route::post('users/store',[UserController::class,'store'])->name('userstore');
+
 
 Route::group(['prefix'=> 'auth'], function(){
     Route::get('logout',[PostController::class,'logout'])->name('logout');
